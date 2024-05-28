d5dedd86d18de767600cbc376050aa9ce80fa881
PIETRA PAULA VOLPATO <23111825@dsn127m1242313.cfp127aluno.local>
2024-05-28 10:01:47 -0300

    Projeto finalizado

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("kotlin-kapt")
+
 }
 
 android {
@@ -64,7 +66,7 @@ dependencies {
    implementation("androidx.navigation:navigation-compose:2.7.7")
    implementation("androidx.room:room-runtime:2.6.1")
    annotationProcessor("androidx.room:room-compiler:2.6.1")
-
+    kapt("androidx.room:room-compiler:2.6.1")
 
 
 
